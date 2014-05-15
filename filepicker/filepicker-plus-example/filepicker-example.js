df53a3c49e10033278b1177fc572704a6eba8279
Nathan Strauser <nathan.strauser@gmail.com>
2014-05-15 16:03:49 -0400

    filepicker

--- filepicker/filepicker-plus-example/filepicker-example.js
+++ filepicker/filepicker-plus-example/filepicker-example.js
@@ -1,19 +1,50 @@
+Images = new Meteor.Collection('images');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to filepicker-example.";
+  Template.myImage.created = function(){
+    loadFilePicker();
   };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.myImage.events({
+    'click #upload': function () {
+      filepicker.pick(
+        {
+          mimetypes: ['image/gif','image/jpeg','image/png'],
+          multiple: false
+        },
+        function(InkBlob){
+          var image = Images.findOne({userId:Meteor.userId()});
+          if(image){
+            Images.update({_id:image._id},
+            {
+              $set:{
+                filepickerId:_.last(InkBlob.url.split("/"))
+              }  
+            });
+          }else{
+            Images.insert({
+              userId:Meteor.userId(),
+              filepickerId:_.last(InkBlob.url.split("/")),
+              createdAt:new Date() //this isnt guarnteed accurate, but its ok for this simple demo
+            });
+          }
+        },
+        function(FPError){
+          alert(FPError.toString());
+        }
+      );
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.myImage.helpers({
+    'image': function () {
+      return Images.findOne({userId:Meteor.userId()});
+    }
+  });
+
+  Template.profileImages.helpers({
+    'images': function () {
+      return Images.find({},{sort:{createdAt:-1},limit:100});
+    }
   });
 }
